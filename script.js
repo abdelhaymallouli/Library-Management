cdb17f500a324a0d81ac65b31c64744f2f8cb3bb
Abdelhay Mallouli <abdelhay.mallouli@gmail.com>
2024-10-31 09:52:53 +0100

    .

--- script.js
+++ script.js
@@ -1,94 +1,104 @@
-// CLASS BOOK 
+// CLASS BOOK
 class Book {
-    constructor(title, author, year, price) {
+    constructor(title, author, year, price){
         this.title = title;
         this.author = author;
         this.year = year;
         this.price = price;
     }
-    // function add book HAS THE HTML CODE
+
+    // function add book to the table
     addBook() {
-        return `
+        return`
                 <td>${this.title}</td>
                 <td>${this.author}</td>
                 <td>${this.year}</td>
-                <td>$${this.price}</td>
+                <td>${this.price}</td>
                 <td>
-                    <button class="edit-button"><img src="img/add.svg" alt="add"></button>
-                    <button class="delete-button"><img src="img/x-mark.png" alt=""></button>
+                    <button class="edit-buttom">Edit</button>
+                    <button class="delete-buttom">Delete</button>
                 </td>
         `;
     }
 }
 
 const books = [];
-const form = document.getElementById('bookForm');
+const form = document.getElementById('Form');
 const table = document.querySelector('#Table tbody');
-let editing = -1;
+let edit = -1;
 
-// WHEN "SUBMIT" RUN THIS
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+// "submit" run this
+form.addEventListener('submit', function(add){
+    add.preventDefault();  // this is stop the submit
 
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const year = document.getElementById('year').value;
     const price = document.getElementById('price').value;
-    // if there is no book add a book
-    if (editing === -1) { 
-        const newBook = new Book(title, author, year, price);
-        const newRowElement = document.createElement('tr');
-        newRowElement.innerHTML = newBook.addBook();
 
-        buttoms(newRowElement, newBook);
+    // if there is no book in the table add a one
+    if (edit === -1){
+        const Newbook = new Book(title, author, year, price);
+        const newElemnt = document.createElement('tr');
+        newElemnt.innerHTML = Newbook.addBook();
 
-        table.appendChild(newRowElement);
-        books.push(newBook);
-        // if there is a book edit in the form
-    } else {
-        books[editing].title = title;
-        books[editing].author = author;
-        books[editing].year = year;
-        books[editing].price = price;
+        setupButtons(newElemnt, Newbook);
 
-        const updatedRow = document.createElement('tr');
-        updatedRow.innerHTML = books[editing].addBook();
-        // update the table 
-        table.replaceChild(updatedRow, table.children[editing]);
-        buttoms(updatedRow, books[editing]);
-        // resiting the editing 
-        editing = -1;
+        table.appendChild(newElemnt);
+        books.push(Newbook);
+    }
+    // if there is a book in the table add or edit a one.
+    else {
+        books[edit].title = title;
+        books[edit].author = author;
+        books[edit].year = year;
+        books[edit].price = price;
+
+        const updated = document.createElement('tr');
+        updated.innerHTML = books[edit].addBook();
+
+        // update the table
+        table.replaceChild(updated, table.children[edit]);
+        setupButtons(updated, books[edit]);
+        // initial the edit to -1
+        edit = -1;
     }
 
+    // reset the form 
     form.reset();
-    form.querySelector('button[type="submit"]').textContent = 'Add Book';
+    form.querySelector('button[type="submit"]').textContent = 'Add a Book';
+
 });
 
-
-function buttoms(rowElement, book) {
+function setupButtons(Element, book){
     // delete buttom
-    rowElement.querySelector('.delete-button').addEventListener('click', function() {
-        table.removeChild(rowElement);
+    Element.querySelector('.delete-buttom').addEventListener('click', function(){
+        table.removeChild(Element);
         books.splice(books.indexOf(book), 1);
     });
-    // update buttom
-    rowElement.querySelector('.edit-button').addEventListener('click', function() {
-        edit(book);
+
+    // edit buttom
+    Element.querySelector('.edit-buttom').addEventListener('click', function() {
+        editor(book);
     });
 }
 
-// edit buttom add/ Update
-function edit(book) {
+// fonction that show the update buttom
+function editor(book) {
     document.getElementById('title').value = book.title;
     document.getElementById('author').value = book.author;
     document.getElementById('year').value = book.year;
     document.getElementById('price').value = book.price;
 
-    editing = books.indexOf(book);
-    form.querySelector('button[type="submit"]').textContent = 'Update Book';
+    edit = books.indexOf(book);
+    form.querySelector('button[type="submit"]').textContent = 'Update book';   
 }
 
-form.querySelector('button[type="reset"]').addEventListener('click', function() {
-    editing = -1;
+// reset buttom 
+form.querySelector('button[type="reset"]').addEventListener('click', function(){
+    edit = -1;
     form.querySelector('button[type="submit"]').textContent = 'Add Book';
 });
+
+
+
